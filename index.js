14fd87194231460efe5ffdc9f81dd7335007454b
AYANA YAEGASHI <ayaegashi@college.harvard.edu>
2022-09-09 13:51:08 -0400

    back to 3000

--- index.js
+++ index.js
@@ -19,30 +19,30 @@ app.use(express.urlencoded({ extended: true }));
 // connection to db
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
     console.log("Connected to db!");
-    app.listen(process.env.PORT, () => console.log("Server up and running"));
+    app.listen(process.env.PORT || 3000, () => console.log("Server up and running"));
 });
 
 // view engine configuration
 app.set("view engine", "ejs");
 
 // GET METHOD
-app.get("/a", (req, res) => {
+app.get("/", (req, res) => {
     TodoTask.find({}, (err, tasks) => {
         res.render("todo.ejs", { todoTasks: tasks });
     });
 });
 
 // POST method
-app.post('/a',async (req, res) => {
+app.post('/',async (req, res) => {
     const todoTask = new TodoTask({
         content: req.body.content
     });
     try {
         await todoTask.save();
-        res.redirect("/a");
+        res.redirect("/");
     } catch (err) {
         console.log("fail")
-        res.redirect("/a");
+        res.redirect("/");
     }
 });
 
@@ -59,7 +59,7 @@ app
     const id = req.params.id;
     TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
         if (err) return res.send(500, err);
-        res.redirect("/a");
+        res.redirect("/");
     });
 });
 
@@ -68,6 +68,6 @@ app.route("/remove/:id").get((req, res) => {
     const id = req.params.id;
     TodoTask.findByIdAndRemove(id, err => {
         if (err) return res.send(500, err);
-        res.redirect("/a");
+        res.redirect("/");
     });
 });
